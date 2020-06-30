d1d44fde7e4f538613f3951e0b55807fd6c8d9cb
Yvette <nchombuayvta@gmil.com>
2020-06-30 12:05:56 +0200

    Cors added

--- server.js
+++ server.js
@@ -1,1 +1,54 @@
-const express = require ("express")
+const express = require("express")
+const cors = require("cors")
+const { join } = require("path")
+// const listEndpoints = require("express-list-endpoints")
+const helmet = require("helmet")
+
+const booksRouter = require("./src/services/books")
+const {
+  notFoundHandler,
+  badRequestHandler,
+  genericErrorHandler,
+} = require("./src/errorHandler")
+
+const server = express()
+
+
+
+// MIDDLEWARES
+const staticFolderPath = join(__dirname, "../public")
+server.use(express.static(staticFolderPath))
+server.use(express.json())
+
+const whitelist =
+  process.env.NODE_ENV === "production"
+    ? [process.env.FE_URL]
+    : ["http://localhost:3000", "http://localhost:3002"]
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error("Not allowed by CORS"))
+    }
+  },
+}
+
+server.use(cors(corsOptions))
+
+server.use(helmet())
+
+//ROUTES
+server.use("/books", booksRouter)
+
+// ERROR HANDLERS
+server.use(badRequestHandler)
+server.use(notFoundHandler)
+server.use(genericErrorHandler)
+
+//console.log(listEndpoints(server))
+const port = process.env.PORT || 3000
+server.listen(port, () => {
+  console.log("Running on port", port)
+})
--- src/services/books/index.js
+++ src/services/books/index.js
@@ -1,4 +1,133 @@
 const express = require("express")
-const {join} = require("path")
-const{writeDB,readDB} = require("../../utilities")
+const path = require("path")
+const { check, validationResult, sanitizeBody } = require("express-validator")
+const fs = require("fs-extra")
+const multer = require("multer")
+const { join } = require("path")
+const { readDB, writeDB } = require("./utilities")
+
+const booksJsonPath = path.join(__dirname, "books.json")
+
+const booksFolder = join(__dirname, "../../../public/img/books/")
+const upload = multer({})
 const booksRouter = express.Router()
+
+booksRouter.get("/", async (req, res, next) => {
+  try {
+    const data = await readDB(booksJsonPath)
+
+    res.send({ numberOfItems: data.length, data })
+  } catch (error) {
+    console.log(error)
+    const err = new Error("While reading books list a problem occurred!")
+    next(err)
+  }
+})
+
+booksRouter.get("/:asin", async (req, res, next) => {
+  try {
+    const books = await readDB(booksJsonPath)
+    const book = books.find((b) => b.asin === req.params.asin)
+    if (book) {
+      res.send(book)
+    } else {
+      const error = new Error()
+      error.httpStatusCode = 404
+      next(error)
+    }
+  } catch (error) {
+    console.log(error)
+    next("While reading books list a problem occurred!")
+  }
+})
+
+booksRouter.post(
+  "/",
+  [
+    check("asin").exists().withMessage("You should specify the asin"),
+    check("title").exists().withMessage("Title is required"),
+    check("category").exists().withMessage("Category is required"),
+    check("img").exists().withMessage("Img is required"),
+    sanitizeBody("price").toFloat(),
+  ],
+  async (req, res, next) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      const error = new Error()
+      error.httpStatusCode = 400
+      error.message = errors
+      next(error)
+    }
+    try {
+      const books = await readDB(booksJsonPath)
+      const asinCheck = books.find((x) => x.asin === req.body.asin) //get a previous element with the same asin
+      if (asinCheck) {
+        //if there is one, just abort the operation
+        const error = new Error()
+        error.httpStatusCode = 400
+        error.message = "ASIN should be unique"
+        next(error)
+      } else {
+        books.push(req.body)
+        await writeDB(booksJsonPath, books)
+        res.status(201).send("Created")
+      }
+    } catch (error) {
+      next(error)
+    }
+  }
+)
+
+booksRouter.put("/:asin", async (req, res, next) => {
+  try {
+    const books = await readDB(booksJsonPath)
+    const book = books.find((b) => b.asin === req.params.asin)
+    if (book) {
+      const position = books.indexOf(book)
+      const bookUpdated = { ...book, ...req.body } // In this way we can also implement the "patch" endpoint
+      books[position] = bookUpdated
+      await writeDB(booksJsonPath, books)
+      res.status(200).send("Updated")
+    } else {
+      const error = new Error(`Book with asin ${req.params.asin} not found`)
+      error.httpStatusCode = 404
+      next(error)
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
+booksRouter.delete("/:asin", async (req, res, next) => {
+  try {
+    const books = await readDB(booksJsonPath)
+    const book = books.find((b) => b.asin === req.params.asin)
+    if (book) {
+      await writeDB(
+        booksJsonPath,
+        books.filter((x) => x.asin !== req.params.asin)
+      )
+      res.send("Deleted")
+    } else {
+      const error = new Error(`Book with asin ${req.params.asin} not found`)
+      error.httpStatusCode = 404
+      next(error)
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
+booksRouter.post("/upload", upload.single("avatar"), async (req, res, next) => {
+  try {
+    await fs.writeFile(
+      join(booksFolder, req.file.originalname),
+      req.file.buffer
+    )
+  } catch (error) {
+    next(error)
+  }
+  res.send("OK")
+})
+
+module.exports = booksRouter
